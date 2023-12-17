65c11eb7ec5eb3cab708d545700cffd0e5be6d3d
FancyO <orabaya@outlook.com>
2023-12-17 20:29:54 +0300

    blah

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <p className="text-2xl dark:text-white text-center">
+      <p className="text-1xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
         </Link>
