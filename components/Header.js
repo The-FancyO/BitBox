94c66dee33e25008bbb691e0adcd19474604a657
FancyO <orabaya@outlook.com>
2023-12-18 20:37:06 +0300

    final

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <img src='./logo_bitbox_Scaled.png' width={18} height={16} className='mx-auto'></img>
+      <img src='./logo_bitbox_Scaled.avif' width={18} height={16} className='mx-auto'></img>
       <p className="text-1xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
