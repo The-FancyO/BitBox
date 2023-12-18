2cfc456cc14d7e32ec5184ea7081f930ab09a356
FancyO <orabaya@outlook.com>
2023-12-18 16:12:21 +0300

    testy2

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <img src='https://i.postimg.cc/15VQV4G9/Logo.png' width={18} height={16} className='mx-auto' style={"transform: scale(2)"}></img>
+      <img src='https://i.postimg.cc/15VQV4G9/Logo.png' width={18} height={16} className='mx-auto'></img>
       <p className="text-1xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
