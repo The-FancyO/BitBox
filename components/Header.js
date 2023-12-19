2c71ae93ce8f54bf204799bf4dce6303474be665
FancyO <orabaya@outlook.com>
2023-12-19 14:09:06 +0300

    img_2

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header({ name }) {
     <header className="pt-20 pb-12">
         <Link href="/">
           <a>
-          <img src='https://i.postimg.cc/3w7Z7RXr/bitbox-software-high-resolution-logo-black-transparent.png' width={108} height={54} className='mx-auto'></img>
+          <img src='https://i.postimg.cc/zv5jDfZF/Bit-Box-Logo-Scaled-Trasparent.png' className='mx-auto'></img>
           </a>
         </Link>
     </header>
