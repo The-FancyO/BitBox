440d22a7ccae64eaf34e2bb110a3f9b27fd4d174
FancyO <orabaya@outlook.com>
2023-12-19 06:24:30 +0300

    imagy final test2

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <img src='https://i.postimg.cc/G2RgYw2h/logo-bitbox-Scaled.png' width={108} height={54} className='mx-auto'></img>
+      <img src='https://i.postimg.cc/7Y2WMTbL/Logo.png' width={108} height={54} className='mx-auto'></img>
       <p className="text-1xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
